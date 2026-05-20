5f41b51e9d92a475a7d7666414ffb194746518c0
UU-Necrosis <necrosis@example.com>
2026-05-20 22:45:16 +0700

    Auto-backup: 2026-05-20 22:45:16

--- auth.php
+++ auth.php
@@ -410,7 +410,7 @@ if (isset($_POST['btn-forgot'])) {
             <?php 
             // Nếu có session success hoặc có tham số success từ URL truyền về
             if (isset($_GET['success']) && $_GET['success'] == 'verified') {
-                $success = "Kích hoạt tài khoản thành công! Quý khách có thể đăng nhập ngay bây giờ.";
+                $success = "Kích hoạt tài khoản thành công! Quý khách vui lòng đăng nhập lại.";
             }
             if (!empty($success)): 
             ?>
