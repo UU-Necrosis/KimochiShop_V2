ebca90550222727a66035f734f382e563a3ee4e7
UU-Necrosis <necrosis@example.com>
2026-05-20 17:56:56 +0700

    Auto-backup: 2026-05-20 17:56:56

--- auth.php
+++ auth.php
@@ -257,7 +257,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btn-login'])) {
         
         <div class="auth-header">
             <a href="index.php" class="back-to-home" title="Quay lại trang chủ">
-                <i class="fa-solid"><span class="fa-right-to-bracket me-2"></span><span class="d-none d-sm-inline small text-center">  Home Page</span></i>
+                <i class="fa-solid fa-house text-secondary" style="font-size: 0.95rem;"></i>
                 <span class="d-none d-sm-inline small fw-semibold text-secondary">Home Page</span>
             </a>
 
