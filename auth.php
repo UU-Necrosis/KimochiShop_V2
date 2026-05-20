549f4d21b806ac556cd71b3d9315349823d59136
UU-Necrosis <necrosis@example.com>
2026-05-20 15:27:57 +0700

    Auto-backup: 2026-05-20 15:27:57

--- auth.php
+++ auth.php
@@ -332,7 +332,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btn-login'])) {
                         <?php endif; ?>
                     </div>
 
-                    <button type="submit" name="btn-register" class="btn btn-pink w-100 fw-bold py-2 mb-3 shadow-sm">Đăng Ký</button>
+                    <button type="submit" name="btn-register" class="btn btn-pink w-100 fw-bold py-2 mb-1 shadow-sm">Đăng Ký</button>
                 </form>
                 
                 <div class="text-center mt-3">
