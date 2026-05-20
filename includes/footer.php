93fbfc338fa974e26aacc144547c419ac76f68af
UU-Necrosis <necrosis@example.com>
2026-05-20 21:12:23 +0700

    Auto-backup: 2026-05-20 21:12:23

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,10 @@
+        
+        <footer class="site-footer mt-4">
+            <div class="container py-3 text-secondary small text-center">
+            <p class="mb-0">&copy; <span id="year"></span> Kimochi Shop - ufatddt@gmail.com</p>
+            </div>
+        </footer>
+        
         <script src="/personal_projects/KimochiShop_V2/assets/vendor/bootstrap/bootstrap.bundle.min.js"></script>
         <script src="/personal_projects/KimochiShop_V2/assets/js/main.js"></script>
     </body>
