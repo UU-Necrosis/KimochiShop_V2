8812b74b14b7133294dae37b14797737fa712657
UU-Necrosis <necrosis@example.com>
2026-05-20 13:53:34 +0700

    Auto-backup: 2026-05-20 13:53:33

--- auth.php
+++ auth.php
@@ -1,5 +1,11 @@
 
 <?php
+
+// được chưa?
+
+
+
+
 // Kiểm tra xem URL đang yêu cầu tab nào (Mặc định là login nếu không có)
 $tab = isset($_GET['tab']) ? $_GET['tab'] : 'login';
 // 1. Nhúng file kết nối database vào
