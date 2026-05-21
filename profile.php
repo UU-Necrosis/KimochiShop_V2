77870a2cb34b933cfe520a3440e1a9d065be71a7
UU-Necrosis <necrosis@example.com>
2026-05-21 07:32:54 +0700

    Auto backup: 07:32:53

--- profile.php
+++ profile.php
@@ -306,8 +306,8 @@ try {
                             <p class="text-secondary small" style="font-size: 12px; margin-top:-5px;">Thay đổi quyền hạn tài khoản trực tiếp (Dùng cho quá trình chạy thử nghiệm Demo đồ án).</p>
                             
                             <select name="role" class="form-select input-custom w-100" style="cursor: pointer;">
-                                <option value="user" <?php echo ($user['role'] === 'user') ? 'selected' : ''; ?>>USER (Khách hàng thông thường)</option>
-                                <option value="admin" <?php echo ($user['role'] === 'admin') ? 'selected' : ''; ?>>ADMIN (Quản trị viên toàn quyền)</option>
+                                <option value="user" <?php echo ($user['role'] === 'user') ? 'selected' : ''; ?>>USER (Khách hàng)</option>
+                                <option value="admin" <?php echo ($user['role'] === 'admin') ? 'selected' : ''; ?>>SELLER (Quản trị viên toàn quyền)</option>
                             </select>
                         </div>
 
