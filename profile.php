06bb09f4ff59460ee150fab35d9a96d61bbc28ee
UU-Necrosis <necrosis@example.com>
2026-05-21 09:34:37 +0700

    Auto backup: 09:34:36

--- profile.php
+++ profile.php
@@ -201,7 +201,7 @@ try {
                 </button>
 
                 <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'seller'): ?>
-                    <button class="sidebar-menu-item " id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="true">
+                    <button class="sidebar-menu-item " id="v-pills-supply-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="true">
                         <i class="fa-solid fa-plus-circle me-1"></i> Đăng sản phẩm mới
                     </button>
 
