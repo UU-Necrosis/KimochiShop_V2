e0e08c1500fe552886b7800c6ea077447b31af9d
UU-Necrosis <necrosis@example.com>
2026-05-21 10:12:13 +0700

    Auto backup: 10:12:13

--- profile.php
+++ profile.php
@@ -371,6 +371,9 @@ try {
                             </tbody>
                         </table>
                     </div>
+                <?php else: ?>
+                    <div class="alert alert-danger m-3">Bạn không có quyền sử dụng chức năng này!</div>
+                <?php endif; ?>
             </div>
 
         </main>
