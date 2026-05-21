02cd58a304ad6e1885b61b9154e1f54e36970712
UU-Necrosis <necrosis@example.com>
2026-05-21 08:20:01 +0700

    Auto backup: 08:20:00

--- auth.php
+++ auth.php
@@ -101,8 +101,8 @@ if (isset($_POST['btn-register'])) {
                     <p style='color: #555; font-size: 14px;'>Cảm ơn bạn vì đã tin tưởng và lựa chọn Kimochi Shop. Mã xác thực tài khoản của bạn là: $otp_code</p>
                     <div style='text-align: center; margin: 35px 0;'>
                         <span style='font-size: 28px; font-weight: bold; letter-spacing: 6px; color: #222; background: #fff5f8; padding: 12px 25px; border-radius: 8px; border: 2px dashed #c0c0c0; display: inline-block;'>
-                            <a href='verify.php' style='color: inherit; text-decoration: underline;'>
-                                Xác thực Mã OTP
+                            <a href='$verify_link' style='color: inherit; text-decoration: underline;'>
+                                Xác nhận Mã OTP
                             </a>
                         </span>
                     </div>
