eb0fd1736341e3c69302841e3fe23545a6939aca
UU-Necrosis <necrosis@example.com>
2026-05-21 09:33:32 +0700

    Auto backup: 09:33:31

--- profile.php
+++ profile.php
@@ -209,7 +209,7 @@ try {
 
                     </a>
 
-                    <button class="sidebar-menu-item " id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="true">
+                    <button class="sidebar-menu-item " id="v-pills-stock-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="true">
                         <i class="fa-solid fa-boxes-stacked me-1"></i> Kho hàng của tôi
                     </button>
                     
